42022ef6b7c67d1ff568d38aeae2f66c0812ff2b
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-28 18:06:39 -0400

    testing on print, insertfront, free

--- main.c
+++ main.c
@@ -6,4 +6,16 @@
 
 int main() {
   printf("\tmyTunes: Project 0\nMKS65 - Systems Level Programming\nKiran Vuksanaj & Hilary Zen\n");
+  struct song_node *list = NULL;
+  printf("\nEmpty List: \n\t");
+  print_list(list);
+  printf("\nInsert {1950 - king princess}: \n\t");
+  list = insert_front("1950","king princess",list);
+  print_list(list);
+  printf("\nInsert {a hisotry - bill wurtz}: \n\t");
+  list = insert_front("a history","bill wurtz",list);
+  print_list(list);
+  printf("\nFree list: \n\t");
+  list = free_list(list);
+  print_list(list);
 }
