17f8cb077da9e0e1d4dfa912d367dee7aeb4dd44
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-28 17:18:47 -0400

    function headers from assignment

--- llist.h
+++ llist.h
@@ -1,4 +1,26 @@
 #ifndef LLIST_H
 #define LLIST_H
 
+struct song_node {
+  char * name;
+  char * artist;
+  struct song_node * next;
+};
+
+struct song_node *insert_front(char * name, char * artist, struct song_node * list);
+
+struct song_node *sort_in(char * name, char * artist, struct song_node * list);
+
+void print_list(song_node *list);
+
+struct song_node *find_song(char * name, char * artist, struct song_node *list);
+
+struct song_node *find_artist(char *artist, struct song_node *list);
+
+struct song_node *rand_song(struct song_node *list);
+
+struct song_node *remove_node(char *name, char *artist struct song_node *list);
+
+struct song_node *free_list(struct song_node *list);
+
 #endif
