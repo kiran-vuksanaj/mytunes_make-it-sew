4ef9164edccacaad5d06b48f588f978a33adef61
Kiran Vuksanaj <kiran-vuksanaj@gmail.com>
2019-10-28 15:23:36 -0400

    header on testing file

---
+++ main.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include"llist.h"
+#include"library.h"
+
+int main() {
+  printf("\tmyTunes: Project 0\nMKS65 - Systems Level Programming\nKiran Vuksanaj & Hilary Zen\n");
+}
