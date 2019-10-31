30de315ae3c272828b809c82f1b6e71c925db7e1
Hilary Zen <hzen00@stuy.edu>
2019-10-31 10:36:43 -0400

    Added headers

--- library.c
+++ library.c
@@ -6,7 +6,7 @@
 #include"library.h"
 #include"llist.c"
 
-void * insert_song(char * name, char * artist, struct song_node *[] lib) {
+void * insert_song(char * name, char * artist, struct song_node ** lib) {
   int list_num = int(artist[0]) - 65;
   struct song_node * list = lib[list_num];
   sort_in(name, artist, list);
--- library.h
+++ library.h
@@ -1,11 +1,11 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
-void * insert_song(char * name, char * artist, struct song_node *[] lib);
+void insert_song(char * name, char * artist, struct song_node ** lib);
 
-struct song_node * find_lib_song(char * name, char * artist, struct song_node *[] lib);
+struct song_node * find_lib_song(char * name, char * artist, struct song_node ** lib);
 
-struct song_node * find_lib_artist(char * name, char * artist, struct song_node *[] lib);
+struct song_node * find_lib_artist(char * name, char * artist, struct song_node ** lib);
 
 void * print_letter(char * letter);
 
@@ -15,8 +15,8 @@ void * print_lib();
 
 struct song_node * shuffle();
 
-struct song_node * remove_song(char *name, char *artist, struct song_node *[] lib);
+struct song_node * remove_song(char *name, char *artist, struct song_node ** lib);
 
-struct song_node * remove_lib(struct song_node *[] lib);
+struct song_node * remove_lib(struct song_node ** lib);
 
 #endif
