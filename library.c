1c13ea4882bddc24785e6ab773dc1f69546bc736
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-01 10:47:30 -0400

    rewrote index determination for insert, print

--- library.c
+++ library.c
@@ -7,16 +7,19 @@
 #include"llist.h"
 
 void insert_song(char * name, char * artist, struct song_node ** lib) {
-  int list_num = (int)(artist[0]) - 97;
-  struct song_node * list = lib[list_num];
+  char index = artist[0] - 'a';
+  if(index < 0 || index >= 26) index = 26; // the default other index
+  struct song_node * list = lib[index];
   sort_in(name, artist, list);
 }
 
-void print_letter(char * letter, struct song_node ** lib) {
-  printf("%c list\n", letter);
-  int i = (int)(letter) - 97;
-  struct song_node * list = lib[i];
+void print_letter(char letter, struct song_node ** lib) {
+  char index = letter - 'a';
+  if(index < 0 || index >= 26) index = 26; // the default exterior letter
+  printf("[%c]:\n\t", letter);
+  struct song_node * list = lib[index];
   print_list(list);
+  printf("\n");
 }
 
 void print_lib(struct song_node ** lib) {
