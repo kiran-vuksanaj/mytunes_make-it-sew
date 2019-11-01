43b8b568fa337419077506bf2dd4e30188427a79
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-01 10:47:41 -0400

    clear library before usage

--- test_library.c
+++ test_library.c
@@ -7,6 +7,8 @@ void test_library(){
   printf("we gonna test the libraryyyyyy dont worryyyyy\n");
 
   struct song_node * library[27];
-  printf("\nEmpty Library: \n\t");
+  short i;
+  for(i = 0; i<27; i++) library[i] = 0; // clear the allocated array
+  printf("\nEmpty Library: \n");
   print_lib(library);
 }
